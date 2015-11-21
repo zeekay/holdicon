88da6a13c4f4a5ea1f06f50ca9de3cbeedddefe0
zeekay <zk@monoid.io>
2015-11-21 00:26:58 -0600

    Remove annoyances.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = "You've been booted off the island."
+var fs   = require('fs')
+var glob = require('glob')
+
+var map = {
+  jQuery: [
+    '**/jquery.js',
+    '**/jquery.min.js'
+  ],
+  Bootstrap: [
+    '**/bootstrap.js',
+    '**/bootstrap.min.js'
+  ]
+}
+
+module.exports = {
+  removeAnnoyance: function(name) {
+    map[name].forEach(function(pattern) {
+      glob(pattern, function(err, files) {
+        files.forEach(function(file) {
+          console.log('removing ' + file + ' without warning')
+          fs.unlink(file, function(err) {
+            if (err) throw err
+            console.log('...ya welcome.')
+          })
+        })
+      })
+    })
+  }
+}
